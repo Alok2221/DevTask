b87419dc8c738be3557d7defe0ff4a6ecbab2eca
azadubiec <aleksander.zadubiec@gmail.com>
2025-04-05 00:18:35 +0200

    Innit Commit

---
+++ src/main/java/com/example/devtask/DevtaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.devtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devtask/DevtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
